afe63f0abdf7ea1aa5ddb6a1f7e9217236bb439e
Mahajabin Tokon <tokonislam1122@gmail.com>
2022-09-30 14:19:34 -0600

    Updated2

--- server.py
+++ server.py
@@ -34,36 +34,40 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
+        # Get path and split it into filename
         self.data = self.data.decode("utf-8")
         headers = self.data.split('\r\n')
         filename = headers[0].split()[1]
-        print(headers)
-        print('Hello123')
-        print(filename)
+
+        # If index 0 of header is not GET reponse with 405
         if headers[0].split()[0] != 'GET':
             response = f'HTTP/1.1 405 Not ALLOWED\r\nContent-Type: text/html\r\n'
         else:
+            # Add index.html to any path that ends with /
             if filename[-1] == '/':
                 filename += 'index.html'
             try:
-                url = 'www' + filename
-                
+                # If path name ends with css, open & read content of file and respond    
                 if filename[-3:] == 'css':
+                    url = 'www' + filename
                     file = open(url)
                     content = file.read()
                     file.close()
                     response = f'HTTP/1.1 200 OK\r\nContent-Type: text/css\r\nContent-Length: {len(content)}\r\n\r\n{content}'
+                # If path starts with /.. respond with 404
                 elif filename[:3] == '/..':
                     response = f'HTTP/1.1 404 Not FOUND\r\nContent-Type: text/html\r\n'
-                else :
+                # If path ends with html, open & read content of file respond 
+                elif filename[-4:] == 'html':
+                    url = 'www' + filename
                     file = open(url)
                     content = file.read()
                     file.close()
-                    response = f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(content)}\r\n\r\n{content}'  
-                # else:
-                #     url = url + '/'
-                #     response =f'HTTP/1.1 301 Moved Permanently\r\nLocation: {url}\r\n'
-                    
+                    response = f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(content)}\r\n\r\n{content}'
+                # If path is none of the above, respond with 301  
+                else:
+                    url = filename + '/'
+                    response =f'HTTP/1.1 301 Moved Permanently\r\nLocation: {url}\r\n'
             except FileNotFoundError:
                 response = f'HTTP/1.1 404 Not FOUND\r\nContent-Type: text/html\r\n'    
         self.request.sendall(bytearray(response,'utf-8'))        
